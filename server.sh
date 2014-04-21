f65b917b0b28492b99cf9f68ce4b9cb567281776
Martin Lensment <mlensment@gmail.com>
2014-04-21 22:01:26 +0300

    Working server example

--- server.sh
+++ server.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-handle_req()
-{
-        read req file proto
-        echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
-        echo -e "<html><h1>Hello World</h1></html>"
-}
-
-typeset -fx handle_req
-
-nc -l -p 8080 -c handle_req
+{ echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c <index.html)\r\n\r\n"; cat index.html; } | nc -l -p 8080
