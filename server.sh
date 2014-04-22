0f38851966dae49528a12073bb0c498f5d663cc3
Martin Lensment <mlensment@gmail.com>
2014-04-22 22:14:53 +0300

    Filter out not workign things# #

--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bla() {
+handle_request() {
   echo $1
 }
 
@@ -9,20 +9,24 @@ serve_static() {
 }
 
 start_server() {
-  # (nc -l -p 9000 -i 1 < ncloop | (read METHOD URI PROTOCOL ; echo "path: $URI") > ncloop) &
+  # (nc -l -p 9000 -i 1 < ncloop | (read METHOD URI PROTOCOL ; echo $URI) > ncloop) & #does not work on my debian
+  # (nc -l -p 9000 -i 1 < <(read METHOD URI PROTOCOL ; echo "path: $URI")) & #does not work at all
 
-  (nc -l -p 9000 -i 1 <(read METHOD URI PROTOCOL ; echo "path: $URI")) &
-
-
-  #serve_static | nc -l -p 9000 -i 1 | (read METHOD URI PROTOCOL ; bla "$URI") &
+  # bit hackish way to pipe incoming request to handle_request method,
+  # but named pipe did not work on my slow debian (fifo) and
+  # process substitution did not give any results at all
+  serve_static | nc -l -p 9000 | (read METHOD URI PROTOCOL; handle_request "$URI") &
   server_process_pid=$!
   echo "starting server"
 }
 
-mkfifo ncloop
+#create fifo for piping network data
+if [ ! -f "/tmp/ncloop" ]; then
+  mkfifo "/tmp/ncloop"
+fi
 
 while true; do
-  echo "things"
+  #echo "things"
   echo $server_process_pid
   #check if pid does not exists
   if [ -z $server_process_pid ]; then
@@ -36,7 +40,6 @@ while true; do
   if (! kill -s 0 $server_process_pid); then
     start_server
   fi
-
 
   sleep 1
 done
