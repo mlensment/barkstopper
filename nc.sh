d16375e2e3f9ec5b98ea0ba699ceeca85b2bbe3f
Martin Lensment <mlensment@gmail.com>
2014-04-22 17:38:46 +0300

    Server stuff

--- nc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-listen_port() {
-  nc -k -l 4444
-}
-
-listen_port
--- server.sh
+++ server.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+bla() {
+  echo $1
+}
+
+serve_static() {
+  echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c <index.html)\r\n\r\n"; cat index.html;
+}
+
 start_server() {
-  { echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c <index.html)\r\n\r\n"; cat index.html; } | nc -l -p 9000 -i 1 &
+  # (nc -l -p 9000 -i 1 < ncloop | (read METHOD URI PROTOCOL ; echo "path: $URI") > ncloop) &
+
+  (nc -l -p 9000 -i 1 <(read METHOD URI PROTOCOL ; echo "path: $URI")) &
+
+
+  #serve_static | nc -l -p 9000 -i 1 | (read METHOD URI PROTOCOL ; bla "$URI") &
   server_process_pid=$!
   echo "starting server"
 }
 
+mkfifo ncloop
+
 while true; do
+  echo "things"
+  echo $server_process_pid
   #check if pid does not exists
   if [ -z $server_process_pid ]; then
     start_server
@@ -20,6 +37,6 @@ while true; do
     start_server
   fi
 
-  echo $server_process_pid
+
   sleep 1
 done
