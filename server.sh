b13cd50df3eb3b277a22889659791dc797f3942f
Martin Lensment <mlensment@gmail.com>
2014-04-22 09:24:52 +0300

    Almost functional bash server

--- server.sh
+++ server.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-{ echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c <index.html)\r\n\r\n"; cat index.html; } | nc -l -p 8080
+start_server() {
+  { echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c <index.html)\r\n\r\n"; cat index.html; } | nc -l -p 8080 -i 1 &
+  server_process_pid=$!
+  echo "starting server"
+}
+
+
+
+while true; do
+  #check if pid does not exists
+  if [ -z $server_process_pid ]; then
+    start_server
+    continue
+  fi
+
+  #PID always exists from this point
+
+  #check if process is not running
+  if (! kill -s 0 $server_process_pid); then
+    start_server
+  fi
+
+  echo $server_process_pid
+  sleep 1
+done
